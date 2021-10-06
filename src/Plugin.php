0afd87be084358d26f65924aca8ae2c367ab0fb6
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2021-10-06 10:18:50 +0200

    chore: cleanup and improve docs for `Plugin.php`

--- src/Plugin.php
+++ src/Plugin.php
@@ -4,51 +4,18 @@ declare(strict_types=1);
 namespace Chialab\Rna;
 
 use Cake\Core\BasePlugin;
-use Cake\Core\PluginApplicationInterface;
-use Cake\Http\MiddlewareQueue;
-use Cake\Routing\RouteBuilder;
 
 /**
- * Plugin for Chialab\Rna
+ * Plugin class.
+ *
+ * Chialab/Rna is a CakePHP plugin to seamlessly integrate with [chialab/rna](https://github.com/chialab/rna) build artifacts.
+ *
+ * @example To use this plugin, in your Application's class `bootstrap` method add the following snippet:
+ *
+ * ```php
+ * $this->addPlugin('Chialab/Rna');
+ * ```
  */
 class Plugin extends BasePlugin
 {
-    /**
-     * Load all the plugin configuration and bootstrap logic.
-     *
-     * The host application is provided as an argument. This allows you to load
-     * additional plugin dependencies, or attach events.
-     *
-     * @param \Cake\Core\PluginApplicationInterface $app The host application
-     * @return void
-     */
-    public function bootstrap(PluginApplicationInterface $app): void
-    {
-    }
-
-    /**
-     * Add routes for the plugin.
-     *
-     * If your plugin has many routes and you would like to isolate them into a separate file,
-     * you can create `$plugin/config/routes.php` and delete this method.
-     *
-     * @param \Cake\Routing\RouteBuilder $routes The route builder to update.
-     * @return void
-     */
-    public function routes(RouteBuilder $routes): void
-    {
-    }
-
-    /**
-     * Add middleware for the plugin.
-     *
-     * @param \Cake\Http\MiddlewareQueue $middleware The middleware queue to update.
-     * @return \Cake\Http\MiddlewareQueue
-     */
-    public function middleware(MiddlewareQueue $middlewareQueue): MiddlewareQueue
-    {
-        // Add your middlewares here
-
-        return $middlewareQueue;
-    }
 }
