c0906c55d9172fdac6e2b47d9f38211dca08a92d
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2021-10-06 11:22:15 +0200

    chore: add incomplete tests

---
+++ tests/TestCase/View/Helper/RnaHelperTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Chialab\Rna\Test\TestCase\View\Helper;
+
+use Chialab\Rna\View\Helper\RnaHelper;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \Chialab\Rna\View\Helper\RnaHelper
+ */
+class RnaHelperTest extends TestCase
+{
+    /**
+     * Test {@see RnaHelper::getAssets()} method.
+     *
+     * @return void
+     *
+     * @covers ::getAssets()
+     */
+    public function testGetAssets()
+    {
+        static::markTestIncomplete();
+    }
+
+    /**
+     * Test {@see RnaHelper::script()} method.
+     *
+     * @return void
+     *
+     * @covers ::script()
+     */
+    public function testScript()
+    {
+        static::markTestIncomplete();
+    }
+
+    /**
+     * Test {@see RnaHelper::css()} method.
+     *
+     * @return void
+     *
+     * @covers ::css()
+     */
+    public function testCss()
+    {
+        static::markTestIncomplete();
+    }
+
+    /**
+     * Test {@see RnaHelper::devServer()} method.
+     *
+     * @return void
+     *
+     * @covers ::devServer()
+     */
+    public function testDevServer()
+    {
+        static::markTestIncomplete();
+    }
+}
